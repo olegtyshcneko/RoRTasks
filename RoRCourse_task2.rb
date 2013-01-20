7bf643837959bd76d1fd43762ac5d1808464b2fe
Oleg Tyshchenko <oleg.tyshchenko@gmail.com>
2013-01-20 10:57:22 +0200

    finished 2 task

--- RoRCourse_task2.rb
+++ RoRCourse_task2.rb
@@ -1,10 +1,12 @@
-class Task
+class Task	
 	def method_to_change
 		puts "Today is #{Time.now.wday}"
-	end	
+	end			
 end
-
 
 def modifyClass(classArg)
-	Task
+	classArg.class_exec { def method_to_change; puts "Today is #{Time.now.year} year"; end } if classArg.method_defined?(:method_to_change)	
 end
+
+modifyClass(Task)
+puts Task.new.method_to_change
